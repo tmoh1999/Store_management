88732346d52ee4506140fe7dd3c0a6f4d1871260
* <mohamedtouabti1999@gmail.com>
2025-11-26 15:02:54 +0100

    added update/remove saleitems code

--- blueprints/api_sales.py
+++ blueprints/api_sales.py
@@ -63,3 +63,99 @@ def getsaleitems(user_id):
               "results": result_list2,
               "total":total
     })
+def validateSaleItem(r):
+	
+   if r.product_id:
+       p = Product.query.filter(Product.product_id==r.product_id,Product.user_id==int(session["user_id"])).first()
+       p.quantity_float=max(p.quantity_float-r.quantity_float,0)
+       ####for each item save reduced pjrchaseitem 
+       purchase_items = PurchaseItems.query.filter(PurchaseItems.product_id == r.product_id).order_by(PurchaseItems.purchase_item_id).all()
+       g=r.quantity_float
+       for batch in purchase_items:
+           if batch.remain_quantity>=g:
+              batch.remain_quantity-=g
+              sale_purchase_usage=SalePurchaseUsage(item_id=r.item_id,purchase_item_id=batch.purchase_item_id,quantity_used=g)
+              db2.session.add(sale_purchase_usage)
+              break
+           else:
+              g-=batch.remain_quantity
+              sale_purchase_usage=SalePurchaseUsage(item_id=r.item_id,purchase_item_id=batch.purchase_item_id,quantity_used=batch.remain_quantity)
+              db2.session.add(sale_purchase_usage)
+              batch.remain_quantity=0
+       db2.session.commit()
+def rollBackSaleItem(item_id):
+    r=SaleItems.query.filter(SaleItems.item_id==item_id).first()
+    if r and r.product_id:
+        p = Product.query.filter(Product.product_id==r.product_id,Product.user_id==int(session["user_id"])).first()
+        p.quantity_float=p.quantity_float+r.quantity_float
+        items_usage=SalePurchaseUsage.query.filter(SalePurchaseUsage.item_id==item_id).all()
+        for u in items_usage:
+              batch=PurchaseItems.query.filter(PurchaseItems.purchase_item_id == u.purchase_item_id).order_by(PurchaseItems.purchase_item_id).first()
+              batch.remain_quantity+=u.quantity_used
+              db2.session.delete(u)
+    db2.session.commit()
+@api_sales_bp.route("/items/update", methods=["GET", "POST"])
+@token_required
+def updatesaleitem(user_id):
+    
+    
+    item_id=int(request.json["item_id"])
+    price=float(request.json["price"])
+    quantity=float(request.json["quantity"])
+    description=request.json["description"]   
+    item=SaleItems.query.filter(SaleItems.item_id==item_id).first()
+    if item:
+       if quantity!=item.quantity_float:
+	       rollBackSaleItem(item_id)
+	       item.quantity_float=quantity
+	       db2.session.flush()
+	       validateSaleItem(item)
+	       db2.session.commit()
+       item.unit_price=price
+       item.description=description
+        
+       db2.session.commit()
+    sale=Sales.query.filter(Sales.sale_id==item.sale_id,Sales.user_id==user_id).first()
+    transaction=Transactions.query.filter(Transactions.sale_id==item.sale_id,Transactions.user_id==user_id).first()
+    results2 = SaleItems.query.filter(SaleItems.sale_id==item.sale_id).all()
+    result_list2=[]
+    total=0
+    for r in results2:
+       total+=r.quantity_float*r.unit_price
+       if r.product_id:
+       	p = Product.query.filter(Product.product_id==r.product_id,Product.user_id==user_id).first()
+       	x={"item_id": r.item_id, "name": p.name, "barcode": p.barcode, "price": r.unit_price,"quantity":r.quantity_float,"description":r.description,"profit":r.profit}
+       else:
+           x={"item_id": r.item_id, "name": "", "barcode": "",  "price": r.unit_price,"quantity":r.quantity_float,"description":r.description,"profit":r.profit}    
+           
+       result_list2.append(x)
+    sale.total_amount=total
+    
+    print(len(results2))
+    if transaction:
+       transaction.amount=total;
+    db2.session.commit()
+    return jsonify({
+              "success": True,
+              "total":total
+    })
+@api_sales_bp.route("/items/<int:item_id>/remove", methods=["GET"])
+@token_required
+def removesaleitem(user_id,item_id):
+    item=SaleItems.query.filter(SaleItems.item_id==item_id).first()
+    if item:
+        rollBackSaleItem(item.item_id)
+        db2.session.delete(item)
+        db2.session.commit()
+        return jsonify({
+              "success": True,
+              "message":"item removed"
+         })
+    return jsonify({
+              "success": False,
+              "message":"Error:item not found"
+     })
+        
+        
+    	
+    	
